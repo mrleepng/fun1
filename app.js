1f30bea2e48f4edce0cf95b532046c8ba893efbd
mrleepng <congly2304@gmail.com>
2026-03-12 12:24:55 +0700

    handle /booking-time

--- app.js
+++ app.js
@@ -1,16 +1,36 @@
-const http = require('http');
+const express = require('express')
 
-const server = http.createServer((req, res) => {
-  console.log(`${req.method} ${req.url}`);
+const app = express()
+const PORT = process.env.PORT || 3000
 
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'application/json');
+// middleware parse json
+app.use(express.json())
 
-  res.end(JSON.stringify({
-    status: "ok"
-  }));
-});
+// trả JSON cho mọi request
+app.get('/api/vhr/utility/v0/utility/84/booking-time', (req, res) => {
+  console.log(req.req.query)
+  res.json({
+    message: null,
+    code: null,
+    data:[
+      {"id":631,"utilityId":84,"fromTime":1773356400000,"toTime":1773360000000,"applyAllDays":true,"dayApplied":null,"isFull":false},
+      {"id":632,"utilityId":84,"fromTime":1773360000000,"toTime":1773363600000,"applyAllDays":true,"dayApplied":null,"isFull":false},
+      {"id":633,"utilityId":84,"fromTime":1773363600000,"toTime":1773367200000,"applyAllDays":true,"dayApplied":null,"isFull":false},
+      {"id":634,"utilityId":84,"fromTime":1773367200000,"toTime":1773370800000,"applyAllDays":true,"dayApplied":null,"isFull":false},
+      {"id":635,"utilityId":84,"fromTime":1773370800000,"toTime":1773374400000,"applyAllDays":true,"dayApplied":null,"isFull":false},
+      {"id":636,"utilityId":84,"fromTime":1773374400000,"toTime":1773378000000,"applyAllDays":true,"dayApplied":null,"isFull":false},
+      {"id":637,"utilityId":84,"fromTime":1773385200000,"toTime":1773388800000,"applyAllDays":true,"dayApplied":null,"isFull":false},
+      {"id":638,"utilityId":84,"fromTime":1773388800000,"toTime":1773392400000,"applyAllDays":true,"dayApplied":null,"isFull":false},
+      {"id":639,"utilityId":84,"fromTime":1773392400000,"toTime":1773396000000,"applyAllDays":true,"dayApplied":null,"isFull":false},
+      {"id":640,"utilityId":84,"fromTime":1773396000000,"toTime":1773399600000,"applyAllDays":true,"dayApplied":null,"isFull":false},
+      {"id":641,"utilityId":84,"fromTime":1773399600000,"toTime":1773403200000,"applyAllDays":true,"dayApplied":null,"isFull":false},
+      {"id":642,"utilityId":84,"fromTime":1773403200000,"toTime":1773406800000,"applyAllDays":true,"dayApplied":null,"isFull":false},
+      {"id":643,"utilityId":84,"fromTime":1773406800000,"toTime":1773410400000,"applyAllDays":true,"dayApplied":null,"isFull":false},
+      {"id":644,"utilityId":84,"fromTime":1773410400000,"toTime":1773414000000,"applyAllDays":true,"dayApplied":null,"isFull":false}
+    ]
+  })
+})
 
-server.listen(3000, '0.0.0.0', () => {
-  console.log('HTTP server running at http://0.0.0.0:3000');
-});
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
